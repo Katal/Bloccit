dfc311bd90bc670376153349acc5bdb0675e60a4
Lea <lea-artis@Leas-MacBook-Pro.local>
2016-01-20 17:33:04 -0800

    Checkpoint 43 voting completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119034225) do
+ActiveRecord::Schema.define(version: 20160121012314) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20160119034225) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.integer  "rating"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -74,6 +76,12 @@ ActiveRecord::Schema.define(version: 20160119034225) do
     t.string   "title"
     t.text     "body"
     t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "severity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -95,6 +103,7 @@ ActiveRecord::Schema.define(version: 20160119034225) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "rating"
   end
 
   create_table "users", force: :cascade do |t|
@@ -105,5 +114,16 @@ ActiveRecord::Schema.define(version: 20160119034225) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
