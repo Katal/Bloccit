815be77a56a8246206c27cdc690319bc2dbb9dae
Lea <lea-artis@Leas-MacBook-Pro.local>
2016-01-28 23:43:40 -0800

    Assignment 42 ratings updated, but need to edit issues with post views

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	has_many :labelings, as: :labelable
     has_many :labels, through: :labelings
-    has_many :ratings
+    belongs_to :rating
 
 	default_scope { order('created_at DESC') }
 
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,20 +5,9 @@ class Rating < ActiveRecord::Base
 
 	enum severity: [ :PG, :PG13, :R ] 
 
-	#why isnt severity showing up in display as a method????
 
 	def self.update_rating(rating)
 		Rating.find_or_create_by(severity: rating.to_i)
-
-		# return Rating.none if rating_string.blank?
-
-		#need to select a Rating.severtity which is essentially a number from an array?
-		#then need to save severity
-
-		#this below likely needs to be edited
-		# rating_string.split(",").map do |rating|
-		# 	Rating.find_or_create_by(name: rating.strip)	
-		# end
 	end
 
 
