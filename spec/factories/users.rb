bebfa3397f0f32d4753dc1c01b1ade72ff9d750e
Lea <lea-artis@Leas-MacBook-Pro.local>
2016-02-02 11:18:58 -0800

    Rake routes error fixed, saving 45 so far

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,12 @@
+puts RandomData.random_sentence
+
 FactoryGirl.define do
-   pw = RandomData.random_sentence
+   
    factory :user do
+   	pw = RandomData.random_sentence
      name RandomData.random_name
      sequence(:email){|n| "user#{n}@factory.com" }
-     password pw
+     password RandomData.random_sentence
      password_confirmation pw
      role :member
    end
