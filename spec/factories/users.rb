984b8a163e962bbeb6d3cd9d759cbf63669be923
Lea <lea-artis@Leas-MacBook-Pro.local>
2016-03-15 18:05:04 -0700

    Checkpoint 45 completed, Public Profiles

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,14 @@
 puts RandomData.random_sentence
 
 FactoryGirl.define do
+	pw = RandomData.random_sentence
    
    factory :user do
-   	pw = RandomData.random_sentence
+   	# pw = RandomData.random_sentence
      name RandomData.random_name
      sequence(:email){|n| "user#{n}@factory.com" }
-     password RandomData.random_sentence
+     # password RandomData.random_sentence
+     password pw
      password_confirmation pw
      role :member
    end
