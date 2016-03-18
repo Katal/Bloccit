fc8c7060a50b7d09407c19af0064527191818747
Lea <lea-artis@Leas-MacBook-Pro.local>
2016-03-18 09:31:09 -0700

    Assignment 45 updated

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	def count_if_any(association)
+		if current_user.send(association).any?
+			return pluralize(current_user.send(association).count, association.to_s) 
+		else
+			return "{user.name} has not submitted any #{association} yet."
+		end
+	end
 end
+
