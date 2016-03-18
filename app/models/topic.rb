f4900d436641c994f14ad30fe58fb08cfa6a5e16
Lea <lea-artis@Leas-MacBook-Pro.local>
2016-03-17 22:25:07 -0700

    Assignment 46 completed, Viewable Scopes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,5 +3,11 @@ class Topic < ActiveRecord::Base
 	has_many :labelings, as: :labelable
     has_many :labels, through: :labelings
 
-    scope :visible_to, -> (user) { user ? all : where(public: true) }
+  
+    scope :publicly_viewable, -> { where(public: true) }
+    scope :privately_viewable, -> { where(public: false) }
+    #need to call and test scopes in console
+
+    scope :visible_to, -> (user) { user ? all : publicly_viewable }
+    #need to put publicly_viewable in visible_to syntax
 end
